d604ad9451164c5f17e3e19dba552c3b3776080d
AngelD89 <angeldeprey@yahoo.com>
2025-05-20 17:42:13 -0400

    1square

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class Square with a private size attribute"""
+
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size):
+        self.__size = size
