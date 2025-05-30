de96a924d485547871a60f946eb2d6f8af909900
AngelD89 <angeldeprey@yahoo.com>
2025-05-30 11:03:14 -0400

    4inherits

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a function that checks for inherited instances."""
+"""Function that checks for inherited instances."""
   
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a subclass.
 
     Args:
-        obj: The object to check.
+        obj: Object to check.
         a_class: The class to compare against.
 
     Returns:
-        True if obj is an instance of a subclass (not the class itself),
+        True if obj is an instance of a subclass,
         otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
