bdb30fe5c22f3bfe387254d9b8a6236477264b01
AngelD89 <angeldeprey@yahoo.com>
2025-06-05 19:32:11 -0400

    7add

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that adds all arguments to a list and saves them to a file.
+"""
+import sys
+from os.path import exists
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+if exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
