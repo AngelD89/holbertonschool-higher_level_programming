383342944a25a3dd33f4477273e57ff5ed84c728
AngelD89 <angeldeprey@yahoo.com>
2025-06-05 18:32:04 -0400

    2append

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for appending a string to a text file (UTF8).
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file and returns the number of characters added."""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
