0ed1a4837ac1b1055e9f6d91515bb00ec614c499
AngelD89 <angeldeprey@yahoo.com>
2025-06-05 18:14:20 -0400

    1write

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Module that writes a string to a text file (UTF8) and returns the number of characters written.
+"""Write a string to a text file (UTF8) and return the number of characters written.
 """
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF-8 text file and return the number of characters written.
+    """Write a string to a UTF8 text file.
 
     Args:
-        filename (str): The name of the file to write to.
-        text (str): The string to write.
+        filename (str): The name of the file.
+        text (str): The content to write.
 
     Returns:
         int: Number of characters written.
