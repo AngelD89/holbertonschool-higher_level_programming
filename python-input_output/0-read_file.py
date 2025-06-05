b7c5ed855394d9b0ebcee254c75f55d00c5ea6a3
AngelD89 <angeldeprey@yahoo.com>
2025-06-05 17:58:29 -0400

    0read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
- #!/usr/bin/python3
- def read_file(filename=""):
-    """Reads a UTF-8 text file and prints its content to stdout."""
-    with open(filename, 'r', encoding='utf-8') as f:
+#!/usr/bin/python3
+"""Module for reading and printing contents of a UTF-8 text file.
+"""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints its contents to stdout."""
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
