ec2ce5fd3cf594cd51d5967f855d29051f071a23
AngelD89 <angeldeprey@yahoo.com>
2025-06-06 12:11:49 -0400

    7add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,7 +2,7 @@
 """Adds all arguments to a list and saves them to add_item.json.
 """
 import sys
-from os.path
+import os.path
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
