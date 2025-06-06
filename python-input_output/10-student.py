5559c24d569fa9950e4c161efbea75b2a554cd9f
AngelD89 <angeldeprey@yahoo.com>
2025-06-06 13:50:25 -0400

    10student

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Module defines a Student class with attribute filtering.
+"""Defines a Student class with optional attribute filtering for JSON.
 """
 
 
 class Student:
-    """Defines a student by first and last name, and age."""
+    """Defines a student by first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a student instance."""
+        """Initialize a Student instance."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representation of the Student instance.
-
-        If attrs is a list of strings, only includ those attributes.
-        Otherwise, return all attributes.
         """
-        if isinstance(attrs, list) and all(type(attr) == str for attr in attrs):
-            return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
+        Return a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, only those attributes will be included.
+        """
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
