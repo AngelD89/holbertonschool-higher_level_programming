ccc695d7f76e4ebac6dba3c9211e3c2f31242f54
AngelD89 <angeldeprey@yahoo.com>
2025-09-09 21:58:59 -0400

    100mycalc

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
 
 def main():
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
-    a_str, op, b_str = sys.argv[1], sys.argv[2], sys.argv[3]
+    a_str, op, b_str = argv[1], argv[2], argv[3]
     ops = {"+": add, "-": sub, "*": mul, "/": div}
 
     if op not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
 
     a = int(a_str)
     b = int(b_str)
