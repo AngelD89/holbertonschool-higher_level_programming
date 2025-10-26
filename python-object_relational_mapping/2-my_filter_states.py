0c49db69437874b949e77bdb8f3a137273d5d848
AngelD89 <angeldeprey@yahoo.com>
2025-10-26 11:15:12 -0400

    2myfilter

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values in the states table
+where name matches the argument
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Get command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name
+    )
+    
+    # Create cursor
+    cursor = db.cursor()
+    
+    # Execute SQL query using format to create the query with user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state_name)
+    cursor.execute(query)
+    
+    # Fetch all results
+    states = cursor.fetchall()
+    
+    # Print results
+    for state in states:
+        print(state)
+    
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
