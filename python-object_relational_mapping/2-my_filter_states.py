0ad7d4a738595ece8689aaad20699926d45048c7
AngelD89 <angeldeprey@yahoo.com>
2025-10-26 11:27:53 -0400

    2myfilter

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Displays all values in the states table of hbtn_0e_0_usa
+Lists all values in the states table of hbtn_0e_0_usa
 where name matches the argument.
 """
 
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to the MySQL database
+    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,18 +23,18 @@ if __name__ == "__main__":
         db=database
     )
 
-    # Create a cursor to execute SQL queries
+    # Create a cursor to execute queries
     cur = db.cursor()
 
-    # Execute the query using format (not parameterized)
+    # Build and execute query (as required, using format)
     query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(state_name)
     cur.execute(query)
 
-    # Fetch and display results
+    # Fetch and print results
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
-    # Clean up
+    # Close cursor and connection
     cur.close()
     db.close()
